97416956e49b72f121f9e91215d297b7bcb84c94
Mahmoud Ashraf <mahmoud612.ashraf@gmail.com>
2023-09-01 06:18:20 +0300

    CodingNinjas Maximum sum of non-adjacent elements

---
+++ Dynamic Programming/CodingNinjas Maximum sum of non-adjacent elements .cpp
@@ -0,0 +1,29 @@
+/*
+ * Problem Link: https://www.codingninjas.com/studio/problems/frog-jump_3621012/
+ * */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int maximumNonAdjacentSum(vector<int> &nums){
+    int n = static_cast<int> (nums.size());
+
+    int beforePrev = nums[0]; // number before the previous
+    int prev = 0; // for previous
+
+    if(n > 1) {
+        prev = max(nums[1],nums[0]);
+    }
+    int current = max(prev, beforePrev);
+
+    for (int index = 2; index < n; ++index) {
+        int take = beforePrev + nums[index];
+        current = max(take,prev);
+
+        beforePrev = prev;
+        prev = current;
+    }
+
+    return current;
+}
