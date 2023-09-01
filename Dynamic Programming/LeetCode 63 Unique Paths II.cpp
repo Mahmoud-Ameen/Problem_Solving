6ab402bea09c31ecd0de5705908065c452c7a57c
Mahmoud Ashraf <mahmoud612.ashraf@gmail.com>
2023-09-02 02:47:16 +0300

    LeetCode 63 Unique Paths II

--- Dynamic Programming/LeetCode 63 Unique Paths II.cpp
+++ Dynamic Programming/LeetCode 63 Unique Paths II.cpp
@@ -17,20 +17,21 @@ public:
             return 0;
 
         dp[rows-1][cols-1] = 1;
-        for (int row = rows - 1; row >= 1 ; --row) {
+        for (int row = rows - 1; row >= 0 ; --row) {
             for (int col = cols - 1; col >= 0; --col) {
                 if(obstacleGrid[row][col] == 1)
                     continue;
 
+                // Number of paths if decided to move Down
                 if(row + 1 < rows)
-                    dp[row][col] += dp[row+1][col]; // Down
+                    dp[row][col] += dp[row+1][col];
 
+                // Number of paths if decided to move Right
                 if(col + 1 < cols)
-                    dp[row][col] += dp[row][col+1]; // Right
+                    dp[row][col] += dp[row][col+1];
             }
         }
 
         return dp[0][0];
     }
 };
-
