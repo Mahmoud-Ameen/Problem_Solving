d3100c4999fd2deca23f4d33d299709ce9078e20
Mahmoud Ashraf <mahmoud612.ashraf@gmail.com>
2023-09-14 07:22:39 +0300

    Codeforces 270D Greenhouse Effect

---
+++ Dynamic Programming/Codeforces 270D Greenhouse Effect.cpp
@@ -0,0 +1,35 @@
+/*
+ * Problem link: https://codeforces.com/contest/699/problem/C.
+ * Rate: 1700?
+ * */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Longest Increasing subsequence
+int lis(vector<int>& arr){
+    int n = int(arr.size());
+
+    vector<int> dp(n,1);
+
+    for (int i = 1; i < n; ++i) {
+        for (int j = 0; j < i; ++j) {
+            if(arr[i] >= arr[j])
+                dp[i] = max(dp[i],1+dp[j]);
+        }
+    }
+
+    return *max_element(dp.begin(),dp.end());
+}
+
+int main(){
+    int n,m;
+    cin >> n >> m;
+
+    vector<int> plants (n);
+    for (int i = 0; i < n; ++i) {
+        double x;
+        cin >>plants[i]>>x;
+    }
+    cout << n - lis(plants) << endl;
+}
