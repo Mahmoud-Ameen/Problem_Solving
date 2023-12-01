e6b6793f76ee73107e391ac8439f73e491be984c
Mahmoud Ashraf <mahmoud612.ashraf@gmail.com>
2023-12-01 08:59:56 +0200

    Codeforces 664A Complicated GCD

---
+++ Number Theory/Codeforces 664A Complicated GCD.cpp
@@ -0,0 +1,20 @@
+/*
+ * Problem link: https://codeforces.com/problemset/problem/664/A
+ * */
+
+#include <bits/stdc++.h>
+#define all(a) a.begin(),a.end()
+#define ll long long
+#define cin(vec) for(auto& _item_ : vec) cin >> _item_
+#define cout(vec) for(auto& _item_ : vec) cout << _item_ << " "
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+
+using namespace std;
+
+int main(){
+    string a,b;
+    cin >> a >> b;
+    if(a == b) cout << a <<endl;
+    else cout << 1 <<endl;
+}
