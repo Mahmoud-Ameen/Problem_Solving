ecaa9c72dc007cb3624cc2496a278bdd00b0165c
Mahmoud Ashraf <mahmoud612.ashraf@gmail.com>
2024-04-13 01:03:08 +0200

    Codeforces 1954C Long Multiplication

---
+++ Codeforces Div2/C.1954 Long Multiplication (1954C).cpp
@@ -0,0 +1,65 @@
+
+#include <bits/stdc++.h>
+
+// Data types/structures
+#define ll long long
+#define ull unsigned long long
+#define V vector
+#define VI vector<int>
+#define VLL vector<ll>
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+
+// Loops/ iterators
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define fr(a,target) for (int a=0;a<target;a++)
+#define frl(a,target) for (ll a=0;a<target;a++)
+#define frr(a,start,target) for (int a=start;a<target;a++)
+
+// input/output
+#define cin(vec) for(auto& ____ : vec) cin >> ____
+#define cout(vec) for(auto& ____ : vec) cout << ____ <<" "
+#define in(i) int i;cin>>i;
+#define inl(i) ll i;cin>>i;
+#define ins(i) string i;cin>>i;
+#define cyes cout << "YES\n";
+#define cno cout << "NO\n";
+
+// Matrix graph traversals
+int dx[] = {0,1,-1,0};
+int dy[] = {1,0,0,-1};
+
+using namespace std;
+
+///////// In the name of Allah the Almighty ////////
+////////////////////// Mahmoud /////////////////////
+
+void Ma7moood() {
+    ins(a)
+    ins(b)
+
+    int mostSignificant = 0, n = (int)a.size();
+    for (int i = 0; i < n; ++i) {
+        if(!mostSignificant && a[i] > b[i]) mostSignificant = 1;
+        else if(!mostSignificant && a[i] < b[i]) mostSignificant = 2;
+
+        else if((mostSignificant == 2 && a[i] < b[i])
+                || (mostSignificant == 1 && a[i] > b[i]))
+            swap(a[i],b[i]);
+    }
+
+    cout << a <<"\n" <<b <<endl;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);;
+
+    int t = 1;
+    cin >> t;
+    while(t--){
+        Ma7moood();
+    }
+}
