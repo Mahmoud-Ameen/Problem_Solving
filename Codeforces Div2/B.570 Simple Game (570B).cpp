abf69e0728fd60a1b6e234fbba962b0da5aa4eb7
Mahmoud Ashraf <mahmoud612.ashraf@gmail.com>
2024-04-17 12:57:35 +0200

    Codeforces 570B Simple Game

---
+++ Codeforces Div2/B.570 Simple Game (570B).cpp
@@ -0,0 +1,69 @@
+/*
+ * Problem link: https://codeforces.com/contest/570/problem/B
+ * */
+
+#include <bits/stdc++.h>
+
+// Data types/structures
+#define ll long long
+#define ull unsigned long long
+#define V vector
+#define VI vector<int>
+#define VLL vector<ll>
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+
+// Loops/ iterators
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define fr(a,target) for (int a=0;a<target;a++)
+#define frl(a,target) for (ll a=0;a<target;a++)
+#define frr(a,start,target) for (int a=start;a<target;a++)
+
+// input/output
+#define cin(vec) for(auto& ____ : vec) cin >> ____
+#define cout(vec) for(auto& ____ : vec) cout << ____ <<" "
+#define in(i) int i;cin>>i;
+#define inl(i) ll i;cin>>i;
+#define ins(i) string i;cin>>i;
+#define cyes cout << "YES\n"
+#define cno cout << "NO\n"
+
+// Matrix graph traversals
+int dx[] = {0,1,-1,0};
+int dy[] = {1,0,0,-1};
+
+
+using namespace std;
+
+///////// In the name of Allah the Almighty ////////
+////////////////////// Mahmoud /////////////////////
+
+void Ma7moood() {
+    inl(n)
+    inl(m)
+
+    ll a;
+    if (n - m > m - 1)
+        a = m + 1;
+    else a = (m-1);
+
+    if(a == 0) a = 1;
+    cout << a << endl;
+    // 1 2 3 4 5 6
+    // 1 2 3 4 5 6 7
+    // right : n - m
+    // left : m - 1
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);;
+
+    int t = 1;
+    cin >> t;
+    while(t--){
+        Ma7moood();
+    }
+}
