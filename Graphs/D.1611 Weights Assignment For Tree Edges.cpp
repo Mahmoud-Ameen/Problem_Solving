bedd23f24933d6a6bb0890bbdedb847ef375d276
Mahmoud Ashraf <mahmoud612.ashraf@gmail.com>
2024-07-01 13:49:14 +0300

    1611D Weights Assignment For Tree Edges

---
+++ Graphs/D.1611 Weights Assignment For Tree Edges.cpp
@@ -0,0 +1,71 @@
+/*
+ * Problem link: https://codeforces.com/problemset/problem/1036/C
+ * */
+
+#include <bits/stdc++.h>
+
+// Data types/structures
+#define ll long long
+#define ull unsigned long long
+#define V vector
+#define VI vector<int>
+#define VLL vector<ll>
+#define PII pair<int,int>
+#define PLL pair<ll,ll>
+
+// Loops/ iterators
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define fr(a,target) for (int a=0;a<target;a++)
+#define frl(a,target) for (ll a=0;a<target;a++)
+#define frr(a,start,target) for (int a=start;a<target;a++)
+
+// input/output
+#define cin(vec) for(auto& ____ : vec) cin >> ____
+#define cout(vec) for(auto& ____ : vec) cout << ____ <<" "
+#define in(i) int i;cin>>i
+#define inl(i) ll i;cin>>i
+#define ins(i) string i;cin>>i
+#define cyes cout << "YES\n"
+#define cno cout << "NO\n"
+
+int dx[] = { 0,1,-1,0 };
+int dy[] = { 1,0,0,-1 };
+
+using namespace std;
+#define int ll
+
+///////// In the name of Allah the Almighty ////////
+////////////////////// Mahmoud /////////////////////
+
+void Ma7moud() {
+    in(n);
+    VI parent(n);
+    cin(parent);
+    parent.insert(parent.begin(),0);
+
+    VI p(n);
+    cin(p);
+
+    VI dist(n+1);
+    dist[p[0]] = 1;
+
+    frr(i,1,n){
+        dist[p[i]] = max(dist[parent[p[i]]]+1,dist[p[i-1]]+1);
+    }
+    frr(i,1,n+1)
+        if(parent[i] != i && dist[i] <= dist[parent[i]])return void(cout << -1<<endl);
+
+    frr(i,1,n+1) cout << dist[i] - dist[parent[i]] <<" ";
+    cout << endl;
+ }
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);;
+
+    in(t);
+    while(t--)
+    Ma7moud();
+}
